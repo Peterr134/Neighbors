5bc273b5aa8ba96649f469da4413895f870732cb
Peterr134 <peterr134@nycstudents.net>
2024-03-19 08:55:07 -0400

    Finished!

--- src/Board.java
+++ src/Board.java
@@ -18,11 +18,75 @@ public class Board {
         }
     }
 
-    public void place(int r, int c, int value){
+    public boolean place(int r, int c, int value){
         if(board[r][c] == 0) {
             board[r][c] = value;
+            return true;
         }else{
             System.out.println("Invalid Move");
+            return false;
+        }
+    }
+
+    public int calcPoints(){
+        int points = 0;
+        //horizontal points
+        int streak = 0;
+        int streakNum = 0;
+        for(int r = 0; r < board.length; r++){
+            streak = 0;
+            streakNum = 0;
+            for(int c = 0; c < board[0].length-1; c++){
+                if(board[r][c] == board[r][c+1]){
+                    streak++;
+                    streakNum = board[r][c];
+                }else{
+                    streak++;
+                    if(streak != 1) {
+                        points += streak * streakNum;
+                    }
+                    streak = 0;
+                }
+                if(c == board[0].length-2){
+                    streak++;
+                    if(streak != 1) {
+                        points += streak * streakNum;
+                    }
+                }
+            }
+        }
+        //vertical points
+        for(int c = 0; c < board[0].length; c++){
+            streak = 0;
+            streakNum = 0;
+            for(int r = 0; r < board.length-1; r++){
+                if(board[r][c] == board[r+1][c]){
+                    streak++;
+                    streakNum = board[r][c];
+                }else{
+                    streak++;
+                    if(streak != 1) {
+                        points += streak * streakNum;
+                    }
+                    streak = 0;
+                }
+                if(r == board.length-2){
+                    streak++;
+                    if(streak != 1) {
+                        points += streak * streakNum;
+                    }
+                    streak = 0;
+                }
+            }
+        }
+        return points;
+    }
+
+    public void fillBoard(){
+        for(int r = 0; r < 5; r++){
+            for(int c = 0; c < 5; c++){
+                board[r][c] = 1;
+            }
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Board board = new Board();
         Dice d10 = new Dice(10);
+        int timesPlaced = 0;
+        while(timesPlaced < 25) {
+            board.printBoard();
+            int roll = d10.roll();
+            System.out.println("The dice rolls " + roll);
+            System.out.println("Where do you want to place the number? (r,c)");
+            Scanner scanner = new Scanner(System.in);
+            String input2 = scanner.nextLine();
+            if(input2.equals("fill")){
+                board.fillBoard();
+                timesPlaced = 25;
+            }else {
+                String[] input = input2.split(",");
+                if (board.place(Integer.parseInt(input[0]), Integer.parseInt(input[1]), roll)) {
+                    timesPlaced++;
+                }
+            }
+        }
+        System.out.println("Placing complete! Now calculating points");
         board.printBoard();
-        int roll = d10.roll();
-        System.out.println("The dice rolls " + roll);
-        System.out.println("Where do you want to place the number? (r,c)");
-        Scanner scanner = new Scanner(System.in);
-        String[] input = scanner.nextLine().split(",");
-        board.place(Integer.parseInt(input[0]),Integer.parseInt(input[1]),roll);
+        System.out.println("You earned " + board.calcPoints() + " points this game");
     }
 }
