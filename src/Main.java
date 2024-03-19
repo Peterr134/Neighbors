b7fcd21fac0a0a1cbf0de22820b0abc6ff9f50b9
Peterr134 <peterr134@nycstudents.net>
2024-03-19 08:58:56 -0400

    Allowed the player to choose the dice roll if playing with someone else

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Board board = new Board();
+        Scanner scanner = new Scanner(System.in);
         Dice d10 = new Dice(10);
         int timesPlaced = 0;
         while(timesPlaced < 25) {
@@ -10,13 +11,16 @@ public class Main {
             int roll = d10.roll();
             System.out.println("The dice rolls " + roll);
             System.out.println("Where do you want to place the number? (r,c)");
-            Scanner scanner = new Scanner(System.in);
+            System.out.println("If you're playing with someone else, then add a 3rd comma to input the dice roll they got");
             String input2 = scanner.nextLine();
             if(input2.equals("fill")){
                 board.fillBoard();
                 timesPlaced = 25;
             }else {
                 String[] input = input2.split(",");
+                try{
+                    roll = Integer.parseInt(input[2]);
+                }catch(ArrayIndexOutOfBoundsException e){}
                 if (board.place(Integer.parseInt(input[0]), Integer.parseInt(input[1]), roll)) {
                     timesPlaced++;
                 }
